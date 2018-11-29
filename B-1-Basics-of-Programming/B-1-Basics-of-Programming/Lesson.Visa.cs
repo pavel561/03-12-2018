0e309a7cbb47b003a6cabd90bfa3477466261eca
Olga R <olga.rondarava@oxagile.com>
2018-11-29 13:29:04 +0300

    B-1-Basics-of-Programming

---
+++ B-1-Basics-of-Programming/B-1-Basics-of-Programming/Lesson.Visa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_1_Basics_of_Programming
+{
+    public partial class Visa
+    {
+        public static void VisaExample()
+        {
+
+        }
+    }
+}
