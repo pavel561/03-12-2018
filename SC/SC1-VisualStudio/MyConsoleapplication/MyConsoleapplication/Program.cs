00381e4a8e958e7afb7fd9505ede27bcc4088744
Olga R <olga.rondarava@oxagile.com>
2018-12-02 16:57:45 +0300

    SC1-VisualSTudio

---
+++ SC/SC1-VisualStudio/MyConsoleapplication/MyConsoleapplication/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyConsoleapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = args[0];
+            var age = args[1];
+
+            Console.WriteLine("Hello world");
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("What is your name?");
+
+            Console.ResetColor();
+
+            //var userName = Console.ReadLine();
+
+            //Console.WriteLine($"Hello {userName}.");
+
+            Console.WriteLine($"Hello {name} of age {age}");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Nice to meet you.");
+
+            Console.ResetColor();
+
+            var resetKey  = Console.ReadKey();
+
+            if (resetKey.Key == ConsoleKey.Delete)
+            {
+                Console.Clear();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
