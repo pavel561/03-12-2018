71cb3b39d30bd8535251d282b0a2687faea30b33
Olga R <olga.rondarava@oxagile.com>
2018-12-03 22:36:23 +0300

    Lesson code

--- B-1-Basics-of-Programming/B-1-Basics-of-Programming/Program.cs
+++ B-1-Basics-of-Programming/B-1-Basics-of-Programming/Program.cs
@@ -10,7 +10,39 @@ namespace B_1_Basics_of_Programming
     {
         static void Main(string[] args)
         {
-            Visa.VisaExample();
+            const int adultage = 18;
+
+            string name = "Ola";
+            var surname = "Rondarava";
+
+            int age = 17;
+
+            string[] documents = new string[]
+            {
+                "Application",
+                "Permission"
+            };
+
+            if (age >= adultage)
+            {
+                Console.WriteLine($"{name} go to window 5");
+            }
+            else
+            {
+                bool hasPermission = documents.Contains("Permission");
+
+                if (hasPermission)
+                {
+                    Console.WriteLine($"{name} go to window 5");
+                    Console.WriteLine(name + " go to window 5");
+                }
+                else
+                {
+                    Console.WriteLine($"{name} go home");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
